c439f6f727cf6b2adf4bc9daabb4ebb70d567a93
coryod14809 <coryod14809@gmail.com>
2017-03-27 18:42:34 -0400

    Add files via upload

---
+++ Unit Testing.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class User:
+    def __init__(self, username, email, ss, hash):
+        self.username = username
+        self.email = Email(email)
+        self.ss = Social(ss)
+        self.hash = Hash(hash)
+
+    def check_password(self, passwrd):
+        return self.hash == Hash(passwrd)
+
+    def __str__(self):
+        return 'Username: ' + self.username + '\n', 'Email address: ' + str(self.email) \
+                + '\n', 'Social Security Number: ' + str(self.ss) + '\n'
